a50672718f5e8022d0ac754385263f3cf2044d32
huyahui <huyahui@codemao.cn>
2020-07-10 21:59:09 +0800

    inits

---
+++ src/main/java/cn/point/cat/middleware /App.java
@@ -0,0 +1,13 @@
+package cn.point.cat.middleware ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
